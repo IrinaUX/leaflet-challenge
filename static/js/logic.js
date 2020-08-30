1a0319bfb1d9120da3fba94259061a7b1c28f15c
Irina Kim <irinahpda@gmail.com>
2020-08-30 22:20:16 +0100

    Add light and dark layout control.

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,13 +4,31 @@ const dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}
   accessToken: API_KEY
 });
 
+// Define variables for our tile layers
+const light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+});
+
 // Creating map object
 const myMap = L.map("map", {
   center: [36.4786667,-117.5141667],
-  zoom: 6
+  zoom: 3
 });
 
 dark.addTo(myMap);
+
+// Only one base layer can be shown at a time
+const baseMaps = {
+  Light: light,
+  Dark: dark
+};
+
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps).addTo(myMap);
 
 function getColor(sig) {
   return sig > 1000 ? 'darkred' :
@@ -27,17 +45,15 @@ const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month
 d3.json(url).then(jsonData => {
   const features = jsonData.features;
   features.forEach(feature => {  
-    let location_sliced = feature.geometry.coordinates.slice(0, 2);
-    let location = location_sliced.reverse();
+    let location = (feature.geometry.coordinates.slice(0, 2)).reverse();
     let type = feature.properties.type;
     let place = feature.properties.place;
     let mag = feature.properties.mag;
     let sig = feature.properties.sig;
-    let timestamp = feature.properties.time;
-    let myDate = new Date(timestamp);
+    let date = new Date(feature.properties.time);
  
     let radius = 0;
-      if (mag > 4.2) {radius = mag * 10000}
+      if (mag > 4) {radius = mag * 40000}
       // else {radius == 800}
 
     const newFeature = L.circle(location, {
@@ -48,7 +64,7 @@ d3.json(url).then(jsonData => {
     });
     
     newFeature.bindPopup(`<h3>${type}: ${place}</h3><hr>
-      <h4>Time: ${myDate}</h4><hr>
+      <h4>Time: ${date}</h4><hr>
       <h4>Magnitude: ${mag}</h4><hr>
       <h4>Significance: ${sig}</h4>`, {maxWidth: 560}) //
     .addTo(myMap)
